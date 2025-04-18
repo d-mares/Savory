e6c48c7faceab5bfdff55134b976467c00fabcea
Daniel Martinez-Escalante <martinezescalanted@tiffin.edu>
2025-04-18 12:17:20 -0400

    collected staticfiles for azure, small setting adjustments

--- savory/settings.py
+++ savory/settings.py
@@ -31,7 +31,6 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -140,6 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
